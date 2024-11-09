5e5cb6bf66fb86752d0a8c73fe93a510abc3489d
darkraven18 <ravi.ujjwal18@gmail.com>
2024-11-09 22:28:54 +0530

    Making changes to the website

--- src/App.js
+++ src/App.js
@@ -1,30 +1,146 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
+import { Bars3Icon } from '@heroicons/react/24/solid';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [currentText, setCurrentText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
+  const toggleMenu = () => {
+    console.log(isMenuOpen)
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    setIsDarkMode(prefersDarkScheme);
+    if (!prefersDarkScheme) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    const checkWidth = () => {
+      console.log('Width change to : ' + window.innerWidth)
+      if (window.innerWidth < 1130) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+        setIsMenuOpen(false);
+      }
+    };
+    checkWidth();
+    window.addEventListener('resize', checkWidth);
+    return () => {
+      window.removeEventListener('resize', checkWidth);
+    };
+  }, [])
+
+
+  
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  useEffect(() => {
+    const phrases = ["Software Developer", "Web Enthusiast", "Creative Coder"];
+    const word=phrases[index%3];
+    async function typeWord(){
+    let curr="";
+    for(const char of word){
+      curr=curr+char;
+      setCurrentText(curr);
+      await delay(120);
+    }
+    await delay(1500);
+    for (let i = 0; i < word.length; i++) {
+      curr = curr.substring(0, curr.length - 1);
+      setCurrentText(curr);
+      await delay(100);
+    }
+    await delay(1000);
+    setIndex(index+1)
+    }
+    typeWord();
+  }, [index]);
+
+
+
   return (
-    <div className="flex flex-col justify-start items-center h-screen bg-black snap-y snap-mandatory overflow-scroll">
-      <div className="z-20 fixed justify-center w-1/2 items-center flex h-[100px]">
-        <div className='cursor-pointer	text-center w-1/4 text-white text-3xl hover:underline underline-offset-8 decoration-green opacity-50 hover:opacity-100 transition-all duration-300'>
-          <b>About</b>
+    <div className="flex flex-col justify-start items-center min-h-screen bg-black dark:bg-white overflow-x-hidden">
+      <div className='basis-[120px] w-full flex flex-row'>
+        <div className='cursor-default font-reenie flex text-green text-8xl font-mono items-center justify-start basis-1/2 pr-4 pl-8 hover:scale-105 transition duration-600 ease-in-out'>
+          &lt;Ujjwal/&gt;
         </div>
-        <div className='cursor-pointer	text-center w-1/4 text-white text-3xl hover:underline underline-offset-8 decoration-green opacity-50 hover:opacity-100 transition-all duration-300'>
-          <b>Profile</b>
+        {isMobile ? (
+          <div data-id="zoomed-button" className='flex text-green flex-row justify-end items-center pr-4 pl-1 basis-1/2'>
+            <Bars3Icon className="opacity-75 cursor-pointer hover:opacity-100 hover:scale-110 transition-colors duration-600 ease-in-out h-6 w-6" onClick={toggleMenu} />
+          </div>
+
+        ) : (
+          <div className='flex text-3xl text-green flex-row justify-end items-center gap-6 pr-4 pl-4 basis-1/2'>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>About</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Skills</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Work</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Contact</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Resume</b></div>
+            <div className={isDarkMode ? 'text-[#F9D71C]' : 'text-black'}>
+              <button onClick={toggleTheme} className="opacity-75 p-2 bg-gray-200 dark:bg-gray-800 rounded-full hover:opacity-100 hover:scale-110 transition-colors duration-600 ease-in-out" aria-label="Toggle theme">
+                {isDarkMode ? (
+                  <SunIcon className="w-6 h-6" />
+                ) : (
+                  <MoonIcon className="w-6 h-6" />
+                )}
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+      {isMenuOpen && (
+        <div className='bg-[#D3D3D3] text-green pt-4 pb-4 gap-4 bg-opacity-25 z-40 w-full h-screen-minus-120'>
+          <div className='flex flex-col justify-center items-center flex-grow'>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>About</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Skills</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Work</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Contact</b></div>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white hover:scale-110 transition duration-600 ease-in-out'><b>Resume</b></div>
+            <div className={isDarkMode ? 'text-[#F9D71C]' : 'text-black'}>
+              <button onClick={toggleTheme} className="opacity-75 p-2 bg-gray-200 dark:bg-gray-800 rounded-full hover:opacity-100 hover:scale-110 transition-colors duration-600 ease-in-out" aria-label="Toggle theme">
+                {isDarkMode ? (
+                  <SunIcon className="w-6 h-6" />
+                ) : (
+                  <MoonIcon className="w-6 h-6" />
+                )}
+              </button>
+            </div>
+          </div>
         </div>
-        <div className='cursor-pointer	text-center w-1/4 text-white text-3xl hover:underline underline-offset-8 decoration-green opacity-50 hover:opacity-100 transition-all duration-300'>
-          <b>Project</b>
+      )}
+      <div className='flex text-green flex-row justify-start items-center w-full h-screen-minus-120'>
+        <div className='text-4xl text-white dark:text-black font-serif basis-2/6 h-full pl-4 flex flex-col justify-center items-center'>
+          <div className='min-w-[30rem] leading-loose'>Hey, I'm <p className='text-green text-5xl font-crimson'> Ujjwal Ravi</p>
+            I'm a <span className="text-green text-5xl font-crimson">{currentText}</span>
+            <span className="text-green text-5xl font-crimson">|</span>
+          </div>
         </div>
-        <div className='cursor-pointer	text-center w-1/4 text-white text-3xl hover:underline underline-offset-8 decoration-green opacity-50 hover:opacity-100 transition-all duration-300'>
-          <b>Contact</b>
+        <div className='flex justify-left items-center basis-3/6 h-full'>
+          <img style={{height:"30rem",width:"30rem"}} src="profile.png" alt="ME!" />
+        </div>
+        <div className='absolute right-0 top-20 flex text-white dark:text-black opacity-5 hover:opacity-20 flex-row justify-end items-start text-[10rem] basis-1/6 h-full inline-block overflow-x-clip'>
+          <span className='-z-50 right-0 font-bold inline-block mr-[-4.5rem] mt-4' style={{ writingMode: "vertical-lr", textOrientation: "sideways" }}>UDEV</span>
         </div>
       </div>
-      <div className='fixed mt-[25vh] snap-normal justify-center w-1/4 items-center flex h-[50vh]'>
-        <b><i className='typewriter text-green text-[200px]'>UR</i></b>
-      </div>
-      <div id='scrool' className="z-3 justify-center snap-start items-start flex w-full min-h-[100vh] h-[100vh] bg-black">
-      </div>
-      <div id='stop' className="z-10 snap-start justify-center items-center flex w-full min-h-[100vh] h-[100vh] bg-green">
-      <b><i className='typewriter2 text-black text-9xl'>Hello World!</i></b>
-      </div>
+      <div className='bg-green w-full h-screen'></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,21 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // This will make Tailwind scan your src directory for classes
   ],
+  darkMode: 'class',
   theme: {
+    extend: {
+      fontFamily: {
+        crimson: ['"Crimson Text"', 'serif'],
+        reenie: ['"Reenie Beanie"', 'cursive'],
+      }
+    },
     colors: {
       transparent: 'transparent',
       'white': '#ffffff',
       'black': '#000608',
       'red': '#6F1A07',
       'green': '#39DFAA'
-    },
+    }, 
   },
   plugins: [],
 };
