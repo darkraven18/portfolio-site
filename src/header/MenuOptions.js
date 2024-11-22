cb1a0b3274c2f0af4fb8a5de7476b57d37ae0540
darkraven18 <ravi.ujjwal18@gmail.com>
2024-11-23 03:45:03 +0530

    Removing git pages integration

--- src/header/MenuOptions.js
+++ src/header/MenuOptions.js
@@ -13,8 +13,8 @@ function MenuOptions({ toggleMenu, option }) {
         }
     }
     return (
-        <a href={link} onClick={buttonClick} className={option === 'Resume' ? 'p-2 border-solid border-2 border-green hover:border-white hover:dark:border-black transition duration-600 ease-in-out hover:scale-110' : 'hover:scale-110 transition duration-600'}>
-            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white transition duration-600 ease-in-out'>
+        <a href={link} onClick={buttonClick} className={option === 'Resume' ? 'p-2 border-solid border-2 border-green hover:border-white dark:border-black transition duration-600 ease-in-out hover:scale-110' : 'hover:scale-110 transition duration-600'}>
+            <div className='cursor-pointer opacity-75 hover:opacity-100 hover:underline underline-offset-4 decoration-white dark:decoration-black transition duration-600 ease-in-out'>
                 <b>
                     {option}
                 </b>
