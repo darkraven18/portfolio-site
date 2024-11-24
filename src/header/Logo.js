2e096e5496da18bfc0f1f511f6b5216cb168ba75
darkraven18 <ravi.ujjwal18@gmail.com>
2024-11-24 15:07:39 +0530

    Removing git page Integration

--- src/header/Logo.js
+++ src/header/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Logo() {
     return (
         <a href='#home' className='cursor-default font-reenie flex text-green text-8xl items-center justify-start basis-1/2 pr-4 pl-[12%] md:pl-[10%] hover:scale-105 transition duration-600 ease-in-out'>
-            &lt;Ujjwals/&gt;
+            &lt;Ujjwal/&gt;
         </a>
     );
 }
